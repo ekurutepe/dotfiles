f4a457438b55a33485fb2ef1c7dc479bd8b7e538
Erik Stromlund <erikstromlund@gmail.com>
2013-09-05 20:48:24 -0700

    Use gst instead of gs; point PROJECTS to ~/dev

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -15,6 +15,6 @@ alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
